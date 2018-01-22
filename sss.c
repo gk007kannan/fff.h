f5aa3b543f506770447ea8ee25c74e4fe6a37f01
gk007kannan <35525377+gk007kannan@users.noreply.github.com>
2018-01-22 16:54:34 +0530

    sss.c

---
+++ sss.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+int hai;
+clrscr(); 
+printf("Enter a number: \n");
+scanf("%d", &num);
+if (hai > 0)
+printf("%d is a positive number \n", hai);
+else 
+if (hai< 0)
+printf("%d is a negative number \n", hai);
+else
+printf("0 is neither positive nor negative");
+getch();
+}
